e8046531d251bf210f11be2d77b232a6caecab58
Hien NGUYEN <hoang-hien.nguyen@cpe.fr>
2020-12-01 10:45:11 +0100

    test

--- pendu spyd.py
+++ pendu spyd.py
@@ -1,29 +1,59 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  1 08:47:20 2020
-
-@author: hienn
-"""
-
 from random import randint
 
-mots = ["bison","alpine","accablant","anticorps","dimension","habitacle","dictionnaire","disproportion","anticonstitutionnellement"]
+def choix_mot():
+    #retourne un mot au hasard dans mots.txt
+    fichier = open('mots.txt','r')
+    contenu = fichier.readline()
 
-
-def choix_mot(liste_mots):
-    n=randint(0,len(liste_mots))
-    mot =liste_mots[n-1]
+    liste_mots = contenu.split(' ')
+    n = randint(0,len(liste_mots))
+    mot = liste_mots[n-1]
     return mot
 
-def pendu(liste_mots,essais):
-    choix=choix_mot(liste_mots)
-    cache=""
-    for i in choix:
-        cache=cache + " _ "
+choix = choix_mot()
+tentatives = 7
+affichage = ""
+lettres_trouvees = ""
 
-    #while essais > 0:
+for l in solution:
+  affichage = affichage + "_ "
 
+print(">> Bienvenue dans le pendu <<")
 
-    print(" notre pendu ")
+while tentatives > 0:
+  print("\nMot à deviner : ", affichage)
+  proposition = input("proposez une lettre : ")[0:1].lower()
 
-print (choix_mot(mots))
+  if proposition in solution:
+      lettres_trouvees = lettres_trouvees + proposition
+      print("-> Bien vu!")
+  else:
+    tentatives = tentatives - 1
+    print("-> Nope\n")
+    if tentatives==0:
+        print(" ==========Y= ")
+    if tentatives<=1:
+        print(" ||/       |  ")
+    if tentatives<=2:
+        print(" ||        0  ")
+    if tentatives<=3:
+        print(" ||       /|\ ")
+    if tentatives<=4:
+        print(" ||       /|  ")
+    if tentatives<=5:                    
+        print("/||           ")
+    if tentatives<=6:
+        print("==============\n")
+
+  affichage = ""
+  for x in solution:
+      if x in lettres_trouvees:
+          affichage += x + " "
+      else:
+          affichage += "_ "
+
+  if "_" not in affichage:
+      print(">>> Gagné! <<<")
+      break
+     
+print("\n    * Fin de la partie *    ")
