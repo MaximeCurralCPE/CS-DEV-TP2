62ea1ed69baca9f1c40ba295706301f4cb783ced
Hien NGUYEN <hoang-hien.nguyen@cpe.fr>
2020-12-01 09:01:20 +0100

    modif mots

--- pendu spyd.py
+++ pendu spyd.py
@@ -4,3 +4,10 @@ Created on Tue Dec  1 08:47:20 2020
 
 @author: hienn
 """
+
+import random
+
+def choix_mot():
+    fichier = open('mots.txt',"r")
+    for ligne in fichier:
+        print(fichier)
