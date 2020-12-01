82838ecf0979476cc905284e609a0d6f750fa4f5
Hien NGUYEN <hoang-hien.nguyen@cpe.fr>
2020-12-01 10:31:30 +0100

    test

--- pendu spyd.py
+++ pendu spyd.py
@@ -5,9 +5,25 @@ Created on Tue Dec  1 08:47:20 2020
 @author: hienn
 """
 
-import random
+from random import randint
 
-def choix_mot():
-    fichier = open('mots.txt',"r")
-    for ligne in fichier:
-        print(fichier)
+mots = ["bison","alpine","accablant","anticorps","dimension","habitacle","dictionnaire","disproportion","anticonstitutionnellement"]
+
+
+def choix_mot(liste_mots):
+    n=randint(0,len(liste_mots))
+    mot =liste_mots[n-1]
+    return mot
+
+def pendu(liste_mots,essais):
+    choix=choix_mot(liste_mots)
+    cache=""
+    for i in choix:
+        cache=cache + " _ "
+
+    #while essais > 0:
+
+
+    print(" notre pendu ")
+
+print (choix_mot(mots))
