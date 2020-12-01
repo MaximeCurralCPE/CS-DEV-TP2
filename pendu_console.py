2dbceccb4e2d416ad8b20aedb11c8d62be623449
Maxime CURRAL <maxime.curral@cpe.fr>
2020-12-01 08:28:25 +0100

    ajout hien

--- pendu_console.py
+++ pendu_console.py
@@ -1,5 +1,5 @@
 '''
 Sujet : CS-DEV TP 2
-Auteur : Maxime Curral
+Auteur : Maxime Curral et Hien Nguyen
 Date de creation : 01/12/2020
 '''
--- pendu_lib.py
+++ pendu_lib.py
@@ -1,6 +1,6 @@
 '''
 Sujet : CS-DEV TP 2
-Auteur : Maxime Curral
+Auteur : Maxime Curral et Hien Nguyen
 Date de creation : 01/12/2020
 '''
 
