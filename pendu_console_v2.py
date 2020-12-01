31d0bcd775dd2145ae00f03fd0227799d6c0da20
Maxime CURRAL <maxime.curral@cpe.fr>
2020-12-01 11:42:36 +0100

    ajout v2

---
+++ pendu_console_v2.py
@@ -0,0 +1,9 @@
+'''
+Sujet : CS-DEV TP 2
+Auteur : Maxime Curral et Hien Nguyen
+Date de creation : 01/12/2020
+'''
+
+from pendu_lib import choix_mot, check_lettre, affichage, check_input, jeu
+
+jeu()
