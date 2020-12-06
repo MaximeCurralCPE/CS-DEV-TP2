90181c948d061aa657e4677f1f93ae618e752555
Hien NGUYEN <hoang-hien.nguyen@cpe.fr>
2020-12-06 16:45:58 +0100

    add tkinter

---
+++ pendu_tkinter.py
@@ -0,0 +1,12 @@
+from tkinter import Tk
+import tkinter as tk
+
+root=Tk()
+root.title("Pendu")
+root.geometry("900x450")
+root.resizable(width=False, height=False)
+saisie_entry=tk.Entry(root)
+saisie_entry.grid(col)
+saisie_label=tk.Entry(root)
+root.mainloop()
+
