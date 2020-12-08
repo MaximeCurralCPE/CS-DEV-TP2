eea6698055ee511113b7a7e0bb2c6060a5f9f3b4
Maxime CURRAL <maxime.curral@cpe.fr>
2020-12-08 10:36:46 +0100

    avancement tkinter

--- pendu_graphique.py
+++ pendu_graphique.py
@@ -3,8 +3,24 @@ Sujet : CS-DEV TP 2 : pendu (interface graphique)
 Auteur : Maxime Curral
 Date de creation : 01/12/2020
 '''
+from tkinter import Tk, Label, Button, Frame, PhotoImage, Canvas, Entry
+import pendu_graphique_lib
 
-from tkinter import Tk, Label, Button, Frame, PhotoImage, Canvas
+
+def recuperation_lettre():
+    return entree_texte.get()
+
+def affichage_mot(mot):
+    mot = Label(frameAffichage, text = mot)
+    mot.pack()
+
+def affichage_image(numero):
+    image_pendu = PhotoImage(file = 'images/bonhomme' + str(numero) + '.gif')
+    canvas = Canvas(mw, width = 100, height = 100)
+    item = canvas.create_image(0,0, image = image_pendu)
+    canvas.pack(side = 'right', padx = 10, pady = 10)
+
+
 
 
 mw = Tk()
@@ -12,26 +28,28 @@ mw.title('Pendu')
 mw.geometry('1000x300+100+200')
 mw['bg'] = 'mediumaquamarine'
 
-image_pendu = PhotoImage(file = 'bonhomme1.gif')
-
-canvas = Canvas(mw, width = 100, height = 100)
-item = canvas.create_image(0,0, anchor = 'center', image = image_pendu)
-canvas.pack(side = 'right', padx = 10, pady = 10)
-
-frame1 = Frame(mw, relief = 'groove')
-frame1.pack(side = 'left', padx = 10, pady = 10)
-frame1['bg'] = 'red'
-
-frame2 = Frame(mw, relief = 'groove')
-frame2.pack(side = 'left')
 
 
-bouton = Button(frame1, text = "Quitter", fg = 'red', command = mw.destroy)
+frameAffichage = Frame(mw)
+frameAffichage.pack(side = 'top')
+frameAffichage['bg'] = 'red'
+
+frameEntree = Frame(mw)
+frameEntree.pack(side = 'left')
+frameEntree['bg'] = 'red'
+
+entree_texte = Entry(frameEntree)
+entree_texte.pack()
+
+bouton = Button(frameEntree, text = "Proposer", bg = 'red', command = recuperation_lettre)
 bouton.pack()
 
-texte = Label(frame1, text = 'Hello World !')
-texte.pack()
+
+
+affichage('bonjour', [0,4,5])
+affichage_image(6)
 
 
 
 mw.mainloop()
+
